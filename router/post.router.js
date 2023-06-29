5a6b376de922d0d90c9e758909cf9d80ae55bab7
devSrabon <et.srabon@gmail.com>
2023-06-29 16:49:56 +0600

    update url

--- router/post.router.js
+++ router/post.router.js
@@ -4,8 +4,8 @@ const postController = require("../controllers/post.controller");
 const verifyToken = require("../middleware/verifyToken");
 const authorization = require("../middleware/authorization");
 
-router.post("/getPost", postController.getAllPost);
-router.get("/createPost", postController.createPost);
+router.get("/getPost", postController.getAllPost);
+router.post("/createPost", postController.createPost);
 router.patch("/updatePost", postController.updatePost);
 
 module.exports = router;
